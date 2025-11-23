986499814a6410c262447a1e89b5307cc518859c
Tr1avan <127694640+Tr1avan@users.noreply.github.com>
2025-11-23 23:35:55 +0300

    Update Green.cs

--- Lab5/Green.cs
+++ Lab5/Green.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+using System.Data.Common;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Lab5
@@ -10,7 +11,25 @@ namespace Lab5
             int[] answer = null;
 
             // code here
-
+            
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            answer = new int[rows]; 
+            for (int i = 0; i < rows; i++)
+            {
+                int min = matrix[i, 0];
+                int minIndex = 0;
+                for (int j = 1; j < cols; j++)
+                {
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                        minIndex = j;
+                    }
+                }
+                answer[i] = minIndex;
+            }
+            
             // end
 
             return answer;
@@ -19,7 +38,28 @@ namespace Lab5
         {
 
             // code here
-
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for ( int i = 0; i < rows; i++)
+            {
+                int max = matrix[i, 0];
+                int maxIndex = 0;
+                for (int j = 1; j < cols; j++)
+                {
+                    if (matrix[i,j] > max)
+                    {
+                        max = matrix[i, j];
+                        maxIndex = j;
+                    }
+                }
+                for (int j = 0; j < maxIndex; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        matrix[i, j] = (int)Math.Floor((double)matrix[i, j] / max);
+                    }
+                }
+            }
             // end
 
         }
@@ -27,6 +67,29 @@ namespace Lab5
         {
 
             // code here
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            if (n != m || k < 0 || k >= m) return;
+
+            int max = matrix[0, 0];
+            int maxind = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[i, i] > max)
+                {
+                    max = matrix[i, i];
+                    maxind = i;
+                }
+            }
+
+            if (maxind == k) return;
+
+            for (int row = 0; row < n; row++)
+            {
+                (matrix[row, k], matrix[row, maxind]) = (matrix[row, maxind], matrix[row, k]);
+            }
 
             // end
 
@@ -35,6 +98,27 @@ namespace Lab5
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int max = int.MinValue;
+            int maxind = -1;
+
+            if (rows != cols) return;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, i] > max)
+                {
+                    max = matrix[i, i];
+                    maxind = i;
+                }
+
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                (matrix[i, maxind], matrix[maxind, i]) = (matrix[maxind, i], matrix[i, maxind]);
+            }
 
             // end
 
@@ -44,7 +128,39 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
+            int maxSum = 0;
+            int maxInd = -1;
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < m; j++)
+                    if (matrix[i, j] > 0)
+                        sum += matrix[i, j];
 
+                if (sum > maxSum || maxInd == -1)
+                {
+                    maxSum = sum;
+                    maxInd = i;
+                }
+            }
+
+            if (maxInd != -1)
+            {
+                answer = new int[n - 1, m];
+                int rowid = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == maxInd) continue;
+                    for (int j = 0; j < m; j++)
+                    {
+                        answer[rowid, j] = matrix[i, j];
+
+                    }
+                    rowid++;
+                }
+            }
             // end
 
             return answer;
